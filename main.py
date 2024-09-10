55567412813912f973880ecaa33255b3e97861a4
Anton <antonilinskiy8@gmail.com>
2024-09-10 16:21:45 +0300

    changes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
-from token_tg import BOT_TOKEN
 
 
-dp = Dispatcher(BOT_TOKEN)
+bot = Bot('7357533859:AAGC3ouB_xHJDoRp6lRlMM5GPhbmDJ5HpEU')
+dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
